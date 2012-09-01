38da39fbc12af48b898b70341b369dac1c32a6cf
Rubén Caro <ruben@elpulgardelpanda.com>
2012-09-01 22:16:22 +0200

    Primitive rotated backup.

---
+++ rotated_backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+remote_user='miriam'
+remote_host='192.168.1.3'
+dest_path="/media/externo/.backup/viajero"
+remote_run="ssh $remote_user@$remote_host"
+
+sources='/home/ruben/Documentos'
+excludes='--exclude=*~ --exclude=.*~'
+max_snaps=7
+
+mkdir -p $HOME/.backup
+log="$HOME/.backup/rotated_backup.log"
+
+# check if rotation is needed
+$remote_run "ls $dest_path/snap.0/*" # if snap.0 is empty then it's not needed
+if [ '0' = "$?" ]; then
+  echo "Rotating $max_snaps snaps... most recent snap will be saved on snap.0."
+
+  # avoid file not existing errors in the first $max_snaps runs
+  $remote_run "/bin/mkdir -p $dest_path/snap.{0..$max_snaps}" &>> $log
+
+  # rotation freeing snap 0, deleting last snap
+  $remote_run "/bin/rm -rf $dest_path/snap.$max_snaps"
+  for i in $(seq $max_snaps -1 1)
+  do
+    $remote_run "/bin/mv $dest_path/snap.$[${i}-1] $dest_path/snap.${i}" &>> $log
+  done
+fi
+
+# avoid file not existing errors in the first run
+$remote_run "/bin/mkdir -p $dest_path/snap.{0..1}" &>> $log
+
+# transfer files hard-linking snap 0 to snap 1
+/usr/bin/rsync -avxhiz --numeric-ids --delete --delete-excluded --log-file=$log --link-dest=$dest_path/snap.1 $excludes $sources $remote_user@$remote_host:$dest_path/snap.0/

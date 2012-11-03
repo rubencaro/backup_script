225763097e59731a5d8ef8ecdb028b2f97ae5a50
rubencaro <ruben.caro.estevez@gmail.com>
2012-11-03 08:19:27 +0100

    Add limit_secs

--- backup.sh
+++ backup.sh
@@ -5,23 +5,26 @@ remote_user='miriam'
 remote_host='192.168.1.3'
 dest_path="/media/miriam/backup/bigtime/"
 home="/home/ruben"
+limit_secs=120 #43200 # 12h
 sources="$home /etc"
-excludes="--exclude=*~ --exclude=.*~ --exclude=.cache --exclude=Trash --exclude=Ubuntu*One --exclude=.mozilla/firefox"
+excludes="--exclude=.backup --exclude=*~ --exclude=.*~ --exclude=.cache --exclude=Trash --exclude=Ubuntu*One --exclude=.mozilla/firefox"
 
 mkdir -p $home/.backup
 log="$home/.backup/backup.log"
 last="$home/.backup/last_backup"
 lock="/tmp/backup.lock"
 
+echo -n '.' >> $log
+
 # check last backup's ts
 if [ -f $last ]; then
   secs_since_last=$[$(date +%s) - $(date -r $last +%s)]
-  if [[ $[$(date +%s) - $secs_since_last] -lt 43200 ]]; then
-    exit 0
-  fi
+  echo "$secs_since_last secs since last (run above $limit_secs)" >> $log
+  [ $secs_since_last -lt $limit_secs ] && exit 0
 fi
 
 # try to create dest path
+echo "Trying connection..." >> $log
 ssh $remote_user@$remote_host "/bin/mkdir -p $dest_path" &>> $log
 # exit if no ssh connection
 [ $? -ne 0 ] && exit 0
@@ -31,6 +34,7 @@ ssh $remote_user@$remote_host "/bin/mkdir -p $dest_path" &>> $log
 touch $lock
 
 # transfer files
+echo "Transferring files..." >> $log
 /usr/bin/rsync -avxhizu --numeric-ids --delete --delete-excluded --log-file=$log $excludes $sources $remote_user@$remote_host:$dest_path
 
 # all done, mark the ts, release the lock
